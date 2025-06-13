5760c0208c8b4b8981638aeb3a742791ae400591
otito <otitoamuga@gmail.com>
2025-06-12 22:01:29 -0400

    user model complete

--- src/models/User.ts
+++ src/models/User.ts
@@ -5,5 +5,21 @@ import mongoose, { Schema, Document } from "mongoose";
 export interface IUser extends Document {
   username: string;
   avatar: string;
-  
+  bio: string;
+  rating: number;
+  email: string;
 }
+
+// 2. Create the Mongoose schema
+const UserSchema: Schema = new Schema({
+  username: { type: String, required: true },
+  avatar: { type: String },
+  bio: { type: String },
+  rating: { type: Number, default: 0 },
+  email: { type: String, required: true, unique: true }
+}, {
+  timestamps: true
+});
+
+// 3. Export the Mongoose model
+export default mongoose.model<IUser>("User", UserSchema);
